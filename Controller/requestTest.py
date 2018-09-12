f07f7a098d498d60619d0d95a8bf5bea6459457e
tommy3531 <thomas.marler@hotmail.com>
2018-09-11 19:49:15 -0500

    Added Tweepy, requests, numpy, pandas, newsAPI and wrote and few tests for NewsAPI

--- Controller/requestTest.py
+++ Controller/requestTest.py
@@ -1,7 +1,23 @@
 import requests
+import pickle
+from newsapi import NewsApiClient
+import json
 
-URL = "https://jsonplaceholder.typicode.com/posts"
 
-r = requests.get(url=URL)
-data = r.json()
-print(data)
+def all_senators():
+    ALL_SENATOR_URL = "https://api.propublica.org/congress/v1/115/senate/members.json"
+
+    headers = {
+        'X-API-KEY': 'SpzjlPZlkMlPKKGCLQS1OqZtCN96lPl7sszOTKra'
+    }
+    r = requests.get(url=ALL_SENATOR_URL, headers=headers)
+    data = r.json()
+    print(data)
+
+
+def find_news_by_topic(search_term):
+    api = NewsApiClient(api_key='2126949bf7be437480eaf1f2dcf0ce51')
+    a = api.get_everything(q=search_term)
+    print(json.dumps(a, indent=4, sort_keys=True))
+
+find_news_by_topic(search_term='Trump')
