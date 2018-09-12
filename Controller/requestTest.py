5a3a1ddb40510e6ed362df3e94032dc1812e45ed
tommy3531 <thomas.marler@hotmail.com>
2018-09-11 22:20:13 -0500

    Working on NewsAPI: learning about Pickel and creating data structures

--- Controller/requestTest.py
+++ Controller/requestTest.py
@@ -10,14 +10,34 @@ def all_senators():
     headers = {
         'X-API-KEY': 'SpzjlPZlkMlPKKGCLQS1OqZtCN96lPl7sszOTKra'
     }
+
     r = requests.get(url=ALL_SENATOR_URL, headers=headers)
     data = r.json()
     print(data)
 
 
 def find_news_by_topic(search_term):
+    newsarticle = []
     api = NewsApiClient(api_key='2126949bf7be437480eaf1f2dcf0ce51')
-    a = api.get_everything(q=search_term)
-    print(json.dumps(a, indent=4, sort_keys=True))
+    articles = api.get_everything(q=search_term)
+    for article in articles['articles']:
+        author = article['author']
+        description = article['description']
+        title = article['title']
+        newsarticle.append(author)
+        newsarticle.append(description)
+        newsarticle.append(title)
+    outfile = open('NewsArticle', 'wb')
+    pickle.dump(newsarticle, outfile)
+    outfile.close()
+
+
+def un_pickle_file():
+    infile = open('NewsArticle', 'rb')
+    new = pickle.load(infile)
+    infile.close()
+    print(new)
+
 
 find_news_by_topic(search_term='Trump')
+un_pickle_file()
