e954d0f71249d840727f633532fb240125e2dd1c
tommy3531 <thomas.marler@hotmail.com>
2018-09-27 21:59:06 -0500

    Created python object from json response

--- Controller/requestTest.py
+++ Controller/requestTest.py
@@ -2,12 +2,15 @@ import requests
 import pickle
 from newsapi import NewsApiClient
 import json
+from Model.Member import Member
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+import python_jsonschema_objects as pjs
 
 
 def all_senators():
+    memberList = []
     ALL_SENATOR_URL = "https://api.propublica.org/congress/v1/115/senate/members.json"
 
     headers = {
@@ -16,9 +19,24 @@ def all_senators():
 
     r = requests.get(url=ALL_SENATOR_URL, headers=headers)
     data = r.json()
-    print(data)
-
-
+    copyright = data["copyright"]
+    root = Member()
+    root.member_root_node(data["results"])
+    for start in root.member_root:
+        # print(start)
+        root.member_basic_information(data["results"][0]["members"])
+        for item in data['results'][0]['members']:
+            root.member_id = item['id']
+            root.member_title = item['title']
+            root.member_first_name = item['first_name']
+            root.member_last_name = item['last_name']
+            root.member_party = item['party']
+            root.member_twitter_account = item['twitter_account']
+            root.member_facebook_account = item['facebook_account']
+            root.member_govTrack_ID = item['govtrack_id']
+            print(root.member_govTrack_ID)
+        # for member in root.member_basic_info:
+        #     print(member)
 def find_news_by_topic(search_term):
     dotenv_path = join(dirname(__file__), '../apikeys.env')
     newsKey = os.getenv('NEWS_KEY')
@@ -54,6 +72,8 @@ def enviro_variable():
     print(twitter)
 
 
-enviro_variable()
-find_news_by_topic(search_term='Trump')
-un_pickle_file()
+# enviro_variable()
+# find_news_by_topic(search_term='Trump')
+# un_pickle_file()
+
+all_senators()
